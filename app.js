71fe0011b9ec3f436bda5bb49b50a4dfd7c5d0a2
Floyd <foo_32120@yahoo.com>
2016-06-05 18:56:05 -0400

    finished Angular shell now onto Node

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = angular.module('floydNews', ['ui.router']);
 app.controller('MainCtrl', ['$scope', 'posts',
 function($scope, posts  ){
   $scope.test = 'Hello world!'; 
-
+ 
 $scope.posts = [//#1
   {title : 'post 1', upvotes : 5},
   {title : 'post 2', upvotes : 2},
@@ -14,14 +14,14 @@ $scope.posts = [//#1
 $scope.addPost = function() {//#2
     //#4
     if(!$scope.title || $scope.title === '') {return;}
-    $scope.posts.push({
-                       title: $scope.title,
+    $scope.posts.push({title: $scope.title,
                        link: $scope.link,
                        upvotes: 0,
                        comments: [
-                      {author: 'Joe', body: 'Cool post!', upvotes:0},
-                     {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes:0}
-                       ]});
+    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  ]             
+    });
     $scope.posts.posts.posts;  
     $scope.title ='';
     $scope.link ="";
@@ -32,15 +32,22 @@ $scope.incrementUpvotes = function (p) {
 p.upvotes += 1;
 };
  }]);
-
 app.controller('PostsCtrl', [
-    '$scope',
-    '$stateParams',
-    'posts',
-    function($scope, $stateParams, posts){
-        
-        
-    }]);
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+$scope.post = posts.posts[$stateParams.id];
+$scope.addComment = function(){
+  if($scope.body === '') { return; }
+  $scope.post.comments.push({
+    body: $scope.body,
+    author: 'user',
+    upvotes: 0
+  });
+  $scope.body = '';
+};
+}]);
 
  app.factory('posts',[function(){
      var o = {
@@ -58,13 +65,13 @@ app.config([
           url: '/home',
           templateUrl: '/home.html',
           controller: 'MainCtrl'
-        }) 
-       
+        })
         .state('posts', {
-        url: '/posts/{id}', 
+        url: '/posts/{id}',
         templateUrl: '/posts.html',
-        controller: 'PostsCtrl'    
-        });
-                
-     $urlRouterProvider.otherwise('home');
+        controller: 'PostsCtrl'
+});
+        $urlRouterProvider.otherwise('home');
+    }//} remove this extr bracket???
+    
 ]);     
